77d89b74e1391b882e98667b420a3890460a5130
HeitorSouza123 <heitor.canafistula@aluno.ce.gov.br>
2025-10-28 10:40:43 -0300

    add ex7

---
+++ ex7.py
@@ -0,0 +1,7 @@
+peso = float(input("Digite seu peso:"))
+altura = float(input("Digite sua altura:"))
+imc = peso/(altura)**2
+if imc < 18.5:
+    print("Abaixo do peso")
+elif imc (18.5 and 24.9):
+    print("Peso normal")
