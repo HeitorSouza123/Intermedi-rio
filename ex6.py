6a12e2052cc75902e13dec9fe07b462112784859
HeitorSouza123 <heitor.canafistula@aluno.ce.gov.br>
2025-10-28 10:23:17 -0300

    modificação ex6

--- ex6.py
+++ ex6.py
@@ -1,17 +1,17 @@
-dia_semana = int(input("Digite um número de 1 a 7:"))
-if dia_semana ==1:
-    print("Dia 1: Segunda")
-    if 2:
-        print("Dia 2: Terça")
-        if 3:
-            print("Dia 3: Quarta")
-            if 4:
-                print("Dia 4: Quinta")
-                if 5:
-                    print("Dia 5: Sexta")
-                    if 6:
-                        print("Dia 6: Sábado")
-if dia_semana > 7:
-    print("Número invalido")
-if dia_semana < 1:
-    print("Número inválido")
+dia_semana = int(input("Digite um número de 1 a 7: "))
+if dia_semana == 1:
+    print("Dia 1: Domingo")
+elif dia_semana == 2:
+    print("Dia 2: Segunda")
+elif dia_semana == 3:
+    print("Dia 3: Terça")
+elif dia_semana == 4:
+    print("Dia 4: Quarta")
+elif dia_semana == 5:
+    print("Dia 5: Quinta")
+elif dia_semana == 6:
+    print("Dia 6: Sexta")
+elif dia_semana == 7:
+    print("Dia 7: Sábado")
+else:
+    print("Número inválido!")
